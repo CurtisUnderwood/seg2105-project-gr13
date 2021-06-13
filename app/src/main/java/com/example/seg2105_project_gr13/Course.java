e5c240248715fe1ee22e79a3b8c8a4b8d16692c1
victorchen01 <victor.chen1447@gmail.com>
2021-06-12 23:14:52 -0400

    Update Course.java

--- app/src/main/java/com/example/seg2105_project_gr13/Course.java
+++ app/src/main/java/com/example/seg2105_project_gr13/Course.java
@@ -9,20 +9,12 @@ public class Course {
         this.code = code;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void setName(String name) { this.name = name; }
 
-    public void setCode(String code) {
-        this.code = code;
-    }
+    public void setCode(String code) { this.code = code; }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
 
-    public String getCode() {
-        return code;
-    }
+    public String getCode() { return code; }
 
 }
